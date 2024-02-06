ea02629b3ea1322b2f2edf8995eb7c40ebd4edbc
hery Ruiz <heryrdev@gmail.com>
2024-02-06 13:18:52 -0500

    added FAQ page

--- src/pages/Faq/Faq.jsx
+++ src/pages/Faq/Faq.jsx
@@ -4,102 +4,59 @@ import "./styles/Faq.css";
 function Faq() {
   return (
     <>
-      <section id="terms">
-        <div className="terms__content">
-          <div className="terms__heading">
-            <h2>YouCoin Website Terms and Contact</h2>
+      <section id="faq">
+        <div className="faq__content">
+          <div className="faq__heading">
+            <h2>M&H Furniture Frequently Asked Questions</h2>
           </div>
-          <div className="terms__flex">
-            <div className="terms__topics">
-              <p>YouCoin Terms of Use</p>
-              <p>YouCoin Privacy</p>
-              <p>YouCoin Cookies</p>
-              <p>YouCoin Product Terms</p>
-              <p>YouCoin Acceptable Use Policy</p>
-              <p>YouCoin Demo</p>
-              <p>YouCoin Market</p>
-              <p>YouCoin Purchasing Agreement</p>
-              <p>YouCoin Percentage</p>
-              <p>YouCoin Investments</p>
-              <p>YouCoin Extras</p>
-              <p></p>
+          <div className="faq__flex">
+            <div className="faq__topics">
+              <p>Shipping & Delivery</p>
+              <p>Returns & Exchanges</p>
+              <p>Product Care & Maintenance</p>
+              <p>Ordering & Payment</p>
+              <p>Customization Options</p>
+              <p>Assembly Instructions</p>
             </div>
-            <div className="terms__conditions">
-              <p className="terms__last">Last Updated: January 27, 2024</p>
-              <h3>1. YouCoin</h3>
+            <div className="faq__conditions">
+              <p className="faq__last">Last Updated: February 6, 2024</p>
+              <h3>1. Shipping & Delivery</h3>
               <p>
-                Welcome to YouCoin! These Terms of Service ("Terms") govern your
-                relationship with YouCoin and its associated services, features,
-                and platforms. Please carefully read and understand these Terms
-                before using YouCoin. By accessing or using YouCoin, you agree
-                to comply with and be bound by these Terms. If you do not agree
-                with any part of these Terms, please refrain from using YouCoin.
+                We offer nationwide shipping on all our furniture products.
+                Delivery times may vary depending on your location. For more
+                information, please visit our Shipping & Delivery page.
               </p>
-              <h3>2. Account</h3>
+              <h3>2. Returns & Exchanges</h3>
               <p>
-                Account Registration: To access certain features of YouCoin, you
-                may be required to create a user account. You agree to provide
-                accurate, current, and complete information during the
-                registration process. Security: You are responsible for
-                maintaining the confidentiality of your account credentials and
-                for all activities that occur under your account. Notify us
-                immediately of any unauthorized use or suspected security
-                breaches. Account Termination: We reserve the right to terminate
-                or suspend your account at our discretion, without notice, if we
-                believe you have violated these Terms or engaged in fraudulent,
-                illegal, or abusive activities.
+                If you're not completely satisfied with your purchase, we offer
+                a hassle-free returns and exchanges policy. Please see our
+                Returns & Exchanges page for detailed instructions.
               </p>
-              <h3>3. Wallet</h3>
+              <h3>3. Product Care & Maintenance</h3>
               <p>
-                Digital Wallet: YouCoin provides a digital wallet service for
-                the secure storage and management of YouCoins. You are solely
-                responsible for the security of your digital wallet and any
-                transactions conducted. Transactions: You may conduct
-                transactions using YouCoins, subject to applicable fees and
-                limits. It is your responsibility to review and understand these
-                fees and limits before initiating transactions. Security
-                Measures: While we employ industry-standard security measures to
-                protect your account and data, you acknowledge that the security
-                of your account and devices is ultimately your responsibility.
+                Proper care and maintenance are essential to ensure the
+                longevity of your furniture. Refer to our Product Care &
+                Maintenance guide for helpful tips and recommendations.
               </p>
-              <h3>4. Illegal Activities</h3>
+              <h3>4. Ordering & Payment</h3>
               <p>
-                Illegal Activities: You agree not to use YouCoin for any illegal
-                activities, including but not limited to money laundering,
-                fraud, or any other criminal activities. Abuse: You must not
-                engage in any activity that may harm, interfere with, or
-                negatively affect the integrity of YouCoin, its users, or its
-                systems. Compliance: You agree to comply with all applicable
-                laws and regulations in your use of YouCoin.
+                Ordering from M&H Furniture is simple and secure. We accept
+                various payment methods, including credit/debit cards and
+                PayPal. Visit our Ordering & Payment page for more details.
               </p>
-              <h3>5. Privacy Policy</h3>
+              <h3>5. Customization Options</h3>
               <p>
-                At YouCoin, we take your privacy seriously. Our commitment to
-                safeguarding your personal information is outlined in our
-                comprehensive Privacy Policy. By using YouCoin, you expressly
-                acknowledge and agree to the terms and practices described in
-                our Privacy Policy, which is incorporated into these Terms by
-                reference. Our Privacy Policy covers various aspects, including:
-                Information Collection: We detail the types of information we
-                collect when you use YouCoin, such as personal details,
-                transaction data, and device information. Usage of Information:
-                The Privacy Policy outlines how we use the collected
-                information, including for account management, transaction
-                processing, security purposes, and improving our services. Data
-                Protection: We explain the measures we take to protect your
-                data, including encryption, access controls, and regular
-                security audits. Third-Party Sharing: If applicable, we provide
-                information about any sharing of your data with third parties
-                and the reasons behind such sharing. Your Choices: The Privacy
-                Policy highlights your rights and choices regarding your
-                personal information, including how to update or delete your
-                data. Your consent to the collection and use of your information
-                is pivotal to your use of YouCoin. We encourage you to review
-                our Privacy Policy regularly to stay informed about how we
-                handle your data.
+                We offer customization options for many of our furniture pieces,
+                allowing you to personalize your order to suit your unique style
+                and preferences. Contact our customer service team for
+                assistance with customization.
               </p>
-              <h3>6. Contact Information</h3>
-              <p>You may contact us by emailing us at Youcoin@gmail.com.</p>
+              <h3>6. Assembly Instructions</h3>
+              <p>
+                Most of our furniture items require assembly. Detailed assembly
+                instructions are provided with each product. If you need further
+                assistance, please don't hesitate to reach out to us.
+              </p>
             </div>
           </div>
         </div>
