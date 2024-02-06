4c535a524f28549214999ebf7d20b76317b99649
hery Ruiz <heryrdev@gmail.com>
2024-02-06 11:43:12 -0500

    added other product datas

---
+++ src/data/electronics.js
@@ -0,0 +1,17 @@
+const electronics = [
+    {
+      name: "Toaster",
+      price: 1000,
+      image:
+        "https://minimalist-e-commerce.vercel.app/static/media/2.f312c91f99a2c3a60550.jpg",
+      id: 2,
+    },
+    {
+      name: "Secure Cam",
+      price: 20000,
+      image:
+        "https://minimalist-e-commerce.vercel.app/static/media/16.9d0e830b5f094893fe86.jpg",
+      id: 12,
+    }
+  ];
+  export default electronics;
